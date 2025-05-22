a6adb1ca4483afe8f6ee8f67fc36f6b4b587c870
GaneshLathin <150793926+GaneshLathin@users.noreply.github.com>
2025-05-22 15:00:52 +0530

    Update Bubble.cpp

--- Sorting/Bubble.cpp
+++ Sorting/Bubble.cpp
@@ -6,6 +6,19 @@
 // You are using GCC
 #include<iostream>
 using namespace std;
+void anotherway()
+{
+    for(int i=n-1;i>=0;i--)
+    {
+        for(int j=0;j<i;j++)
+        {
+            if(a[j]>a[j+1])
+            swap(a[j],a[j+1]);
+        }
+    }
+                
+            
+}
 int main()
 {
     int n;
